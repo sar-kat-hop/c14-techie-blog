4afd9bec5031ddec8cc729cdd68944283c7ba7a2
Sarah <sarah.k.hopper@gmail.com>
2023-02-11 20:04:05 -0800

    install & init npm modules, add package-lock.json to gitignore

---
+++ server.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const userSess = {
+    secret: 'classified and restricted supersecret',
+    resave: false,
+    saveUninitialized: true,
+    };
+
+app.use(session(userSess));
+
+const hbs = exphbs.create({ helpers }); //need refresher on what this is doing
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+    });
