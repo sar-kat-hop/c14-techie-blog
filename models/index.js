efd3cf6c16f81b5fea290c0d2101953e6490551c
Sarah <sarah.k.hopper@gmail.com>
2023-02-11 23:05:26 -0800

    update /models/index.js

--- models/index.js
+++ models/index.js
@@ -1,3 +1,13 @@
 const User = require('./User');
+const Comment = require('./Comment');
+
+User.hasMany(Comment, {
+    foreignKey: 'user_name',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_name'
+});
 
 module.exports = { User };
