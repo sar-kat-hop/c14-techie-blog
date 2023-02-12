8cdcba54a4bf57be74796518e376561fc84a5b54
Sarah <sarah.k.hopper@gmail.com>
2023-02-11 23:03:19 -0800

    add Comment model

---
+++ models/Comment.js
@@ -0,0 +1,42 @@
+const sequelize = require('../config/connection');
+const { Model, DataTypes } = require('sequelize');
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        body: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        user_name: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'name',
+            },
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment',
+    }
+);
