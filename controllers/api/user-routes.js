5c9458607a5b47a4b0548512d39f13bbf3c588d2
Sarah <sarah.k.hopper@gmail.com>
2023-02-13 20:38:48 -0800

    try to fix login routing issues

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,92 +1,51 @@
-const router = require('express').Router();
-const { User, Comment } = require('../../models');
+const router = require("express").Router();
+const { User, Comment } = require("../../models");
 
-router.get('/', async (req, res) => {
+// const email = document.querySelector(('#login-email').value.trim());
+// const password = document.querySelector(('#login-password').value.trim());
+
+// Login
+router.post("/login", async (req, res) => {
     try {
-        const userData = await User.findAll({
-            include: [
-                {model: Comment, attributes: ['title', 'body', 'date_created']}
-            ],
-            order: ['date_created', 'DESC'],
-        });
+        const userData = await User.findOne({
+            where: {
+                email: req.body.email,
+                },
+            });
 
-        res.status(200).json(userData);
-    } catch (err) {
-        res.status(500).json(err);
-        console.log(err);
-    }
-});
-
-router.get('/:id', async(req, res) => {
-    try {
-        const userData = User.findByPk(req.params.id, {
-            include: [
-                {model: Comment, attributes: ['title', 'body', 'date_created']}
-            ],
-            order: ['date_created', 'DESC'],
-        });
-
-        if(!userData) {
-            res.status(404).json({message: 'Could not find user (ID does not exist)'});
+        if (!userData) {
+            res.status(400).json({ message: "Incorrect email or password. Please try again! 1" });
             return;
         }
 
-        res.status(200).json(userData);
-    } catch(err) {
-        res.status(500).json(err);
-        console.log(err);
-    }
-});
+        const passwordValid = await userData.checkPassword(req.body.password);
 
-// login
-router.post('/login', async (req, res) => {
-    try {
-        // look for user's email in db
-        const userData = await User.findOne({ where: {email: req.body.email} });
-
-        // if user's email not found...
-        if(!userData) {
-            res
-                .status(400)
-                .json({ message: 'Email not found. Please try again.' });
-            return;
-        }
-    
-        // verify password entered matches what's saved in db
-        const passVerified = await userData.checkPassword(req.body.password);
-
-        // if password can't be verified...
-        if(!passVerified) {
-            res .status(400).json({message: 'Password incorrect. Please try again.'});
+        if (!passwordValid) {
+            res.status(400).json({ message: "Incorrect email or password. Please try again! 2" });
             return;
         }
 
-        // create session for logged-in user
         req.session.save(() => {
-            req.session.user_id = userData.id;
             req.session.logged_in = true;
-
-            req.json({ user: userData, message: 'Login successful.'});
+            req.session.userID = userData.id;
+            console.log("login successful");
+            res.json({ user: userData, message: "Logged in successfully" });
         });
-
-    } catch (err) {
-        res.status(400).json(err);
-        console.log(err);
-    }
+        } catch (err) {
+            res.status(500).json(err);
+        }
 });
 
-// logout
-router.post('/logout', (req, res) => {
-    if(req.session.logged_in) {
-        // remove session vars
+// Logout
+router.post("/logout", (req, res) => {
+    if (req.session.loggedIn) {
         req.session.destroy(() => {
-            res.status(204).end();
+        res.status(204).end();
         });
     } else {
         res.status(404).end();
-        console.log('Error logging out');
     }
-});
+    });
 
 // create new user
 // router.post('/', async (req, res) => {
@@ -103,5 +62,84 @@ router.post('/logout', (req, res) => {
 //         res.status(400).json(err);
 //     }
 // });
+
+// user authentication
+// router.post('/login', async (req, res) => {
+//     try {
+//         // look for user's email in db
+//         const userData = await User.findOne({ where: {email: req.body.email} });
+
+//         // if user's email not found...
+//         if(!userData) {
+//             res
+//                 .status(400)
+//                 .json({ message: 'Email not found. Please try again.' });
+//             return;
+//         }
+
+//         // verify password entered matches what's saved in db
+//         const passVerified = await userData.checkPassword(req.body.password);
+
+//         // if password can't be verified...
+//         if(!passVerified) {
+//             res .status(400).json({message: 'Password incorrect. Please try again.'});
+//             return;
+//         }
+
+//         // create session for logged-in user
+//         req.session.save(() => {
+//             req.session.user_id = userData.id;
+//             req.session.logged_in = true;
+
+//             req.json({ user: userData, message: 'Login successful.'});
+//         });
+
+//     } catch (err) {
+//         res.status(400).json(err);
+//         console.log(err);
+//     }
+// });
+
+module.exports = router;
+
+const router = require("express").Router();
+const { User, Post } = require("../../models");
+
+// Sign Up
+router.post("/", async (req, res) => {
+    try {
+        const userData = await User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+        });
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+            res.status(200).json(userData);
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Create new post
+router.post("/post", async (req, res) => {
+    try {
+        const postData = await Post.create({
+            title: req.body.title,
+            content: req.body.body,
+            user_id: req.session.userID,
+        });
+
+        if (!req.session.loggedIn) {
+            res.status(400).json({ message: "Could not log in." });
+        }
+
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
--- public/js/logout.js
+++ public/js/logout.js
@@ -1,6 +1,6 @@
 const logout = async () => {
     // remove session on backend
-    const response = await fetch('/api/users/logout', {
+    const response = await fetch('/api/logout', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
     });
@@ -8,10 +8,26 @@ const logout = async () => {
     if (response.ok) {
       // send to login page on successful logout    
         document.location.replace('/login');
+        // document.location = '/'; 
     } else {
         alert(response.statusText);
         console.log('Error logging out');
     }
 };
 
-document.querySelector('#logout').addEventListener('click', logout);
+document.querySelector('#logout-btn').addEventListener('click', logout);
+
+
+// const logout = async () => {
+//     const response = await fetch("/api/users/logout", {
+//       method: "POST",
+//       headers: { "Content-Type": "application/json" },
+//     });
+  
+//     if (response.ok) {
+//       document.location = "/";
+//     } else {
+//       alert("Failed to log out.");
+//     }
+//   };
+// const logoutBtn = document.querySelector('#logout-btn).click(logout);
