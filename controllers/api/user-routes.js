60abd58a9373210df001bb6a7bbe4124b6322e1d
Sarah <sarah.k.hopper@gmail.com>
2023-02-13 20:44:21 -0800

    clean up user-routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,9 +1,6 @@
 const router = require("express").Router();
 const { User, Comment } = require("../../models");
 
-// const email = document.querySelector(('#login-email').value.trim());
-// const password = document.querySelector(('#login-password').value.trim());
-
 // Login
 router.post("/login", async (req, res) => {
     try {
@@ -48,95 +45,41 @@ router.post("/logout", (req, res) => {
     });
 
 // create new user
-// router.post('/', async (req, res) => {
-//     try {
-//         const userData = await User.create(req.body);
-
-//         req.session.save(() => {
-//             req.session.user_id = userData.id;
-//             req.session.logged_in = true;
-
-//             res.status(200).json(userData);
-//         });
-//     } catch (err) {
-//         res.status(400).json(err);
-//     }
-// });
-
-// user authentication
-// router.post('/login', async (req, res) => {
-//     try {
-//         // look for user's email in db
-//         const userData = await User.findOne({ where: {email: req.body.email} });
-
-//         // if user's email not found...
-//         if(!userData) {
-//             res
-//                 .status(400)
-//                 .json({ message: 'Email not found. Please try again.' });
-//             return;
-//         }
-
-//         // verify password entered matches what's saved in db
-//         const passVerified = await userData.checkPassword(req.body.password);
-
-//         // if password can't be verified...
-//         if(!passVerified) {
-//             res .status(400).json({message: 'Password incorrect. Please try again.'});
-//             return;
-//         }
-
-//         // create session for logged-in user
-//         req.session.save(() => {
-//             req.session.user_id = userData.id;
-//             req.session.logged_in = true;
-
-//             req.json({ user: userData, message: 'Login successful.'});
-//         });
-
-//     } catch (err) {
-//         res.status(400).json(err);
-//         console.log(err);
-//     }
-// });
-
-module.exports = router;
-
-const router = require("express").Router();
-const { User, Post } = require("../../models");
-
-// Sign Up
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
     try {
         const userData = await User.create({
-            username: req.body.username,
             email: req.body.email,
             password: req.body.password,
+            username: req.body.username,
         });
 
         req.session.save(() => {
-            req.session.loggedIn = true;
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
             res.status(200).json(userData);
         });
     } catch (err) {
-        res.status(500).json(err);
+        res.status(400).json(err);
     }
 });
 
 // Create new post
-router.post("/post", async (req, res) => {
+router.post("/comment", async (req, res) => {
     try {
-        const postData = await Post.create({
-            title: req.body.title,
-            content: req.body.body,
+        const commentData = await Comment.create({
             user_id: req.session.userID,
+            title: req.body.title,
+            body: req.body.body,
+            date_created: req.session.date_created,
         });
 
-        if (!req.session.loggedIn) {
-            res.status(400).json({ message: "Could not log in." });
+        if (!req.session.logged_in) {
+            res.status(400).json({ message: "Could not post comment." });
+            console.log('Failed to post comment.');
         }
 
-        res.status(200).json(postData);
+        res.status(200).json(commentData);
     } catch (err) {
         res.status(500).json(err);
     }
