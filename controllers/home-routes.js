3daf4def99ee398971c608f86556c5e21b8b5a73
Sarah <sarah.k.hopper@gmail.com>
2023-04-29 19:18:25 -0700

    add home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,50 +1,75 @@
 const router = require('express').Router();
-const { User, Comment } = require('../models');
-// const userAuth = require('../utils/auth');
+const { User, BlogPost, Comment } = require('../models');
+const userAuth = require('../utils/auth');
 
-// very basic routing for testing
-// router.get('/', async (req,res) => {
-//     res.render('homepage');
-// });
-
-// router.get('/login', async(req,res) => {
-//     res.render('login');
-// });
+//login route
+router.get('/login', (req, res) => {
+    try {
+        if (!req.session.loggedIn) {
+            res.render('login');
+            console.log('\n User not logged in. Rendering login view.');
+            
+        } else {
+            console.log('\n User logged in. Rendering dashboard.');
+            res.redirect('/dashboard');  
+        }
+    } catch (err) {
+        console.log('\n Error with login route: ' + err + '\n')
+    }
+});
 
 router.get('/', async (req, res) => {
     try {
-        const commentData = await Comment.findAll({
-            attributes: { include: ['title', 'body', 'date_created']}, 
-            order: [['date_created', 'DESC']], // order by newest to oldest
-            include: [ // include poster's username
-                {
-                model: User,
-                attributes: ['username'],
-                },
+        const postData = await BlogPost.findAll({
+            include: User,
+        });
+
+        if(!postData) {
+            res.status(401).json({ message: 'No posts found.'});
+            console.log('No posts found.');
+            //still render homepage even if no posts found?
+            res.render('homepage', {
+                loggedIn: req.session.loggedIn,
+            });
+        }
+
+        const blogPosts = postData.map((blogPosts) => {
+            blogPosts.get({ plain: true })
+        });
+
+        res.render('homepage', {
+            blogPosts,
+            loggedIn: req.session.loggedIn,
+        });
+    } catch (err) {
+        res.status(500).json({ message: '\n Error fetching posts on home route: ', err});
+        console.log('\n Could not fetch and map posts on home route: ' + err + '\n')
+    }
+});
+
+//get single post
+router.get('/:id', userAuth, async (req, res) => {
+    try {
+        const blogPost = await BlogPost.findOne({
+            where: { id: req.params.id },
+            include: [
+                User, { model: Comment, include: [User] },
             ],
         });
 
-        const comments = commentData.map((comment) => 
-            comment.get({ plain: true, nest: true })
-            );
-            res.render('homepage', {
-                comments,
-                logged_in: req.session.logged_in,
-            });
-        } catch (err) {
-            res.status(500).json(err);
-            console.log(err);
+        if(!blogPost) {
+            console.log('\n Post not found. Id may not exist or user is not logged in. \n');
+            res.status(401).json({ message: '\n Blog post not found: ', err})
         }
-});
 
-router.get('/login', (req, res) => {
-    // if there's already active session, send req to homepage
-    if (req.session.logged_in) {
-        res.redirect('/');
-        return;
+        const postData = blogPost.get({ plain: true });
+        res.render('blog-post', {
+            loggedIn: req.session.loggedIn,
+            postData,
+        });
+
+    } catch (err) {
+        console.log('\n Error getting single blog post data: ' + err + '\n');
+        res.status(500).json({ message: '\n Post data not found (home-routes, ln57)', err});
     }
-    // if not logged in, render login view
-    res.render('login');
 });
-
-module.exports = router;
