7b4bc5ae96da58e8a324351f7af3b8c0addd420e
Sarah <sarah.k.hopper@gmail.com>
2023-05-01 15:50:49 -0700

    continue trying to get routes to work, add additional contribution resources used to readme

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,35 +3,36 @@ const { User, BlogPost, Comment } = require('../models');
 const userAuth = require('../utils/auth');
 
 //login route
-router.get('/login', (req, res) => {
-    try {
-        if (!req.session.loggedIn) {
-            res.render('login');
-            console.log('\n User not logged in. Rendering login view.');
+// router.get('/login', (req, res) => {
+//     try {
+//         if (!req.session.loggedIn) {
+//             res.render('login');
+//             console.log('\n User not logged in. Rendering login view.');
             
-        } else {
-            console.log('\n User logged in. Rendering dashboard.');
-            res.redirect('/dashboard');  
-        }
-    } catch (err) {
-        console.log('\n Error with login route: ' + err + '\n')
-    }
-});
+//         } else {
+//             console.log('\n User logged in. Rendering dashboard.');
+//             res.redirect('/dashboard');  
+//         }
+//     } catch (err) {
+//         console.log('\n Error with login route: ' + err + '\n')
+//     }
+// });
 
 router.get('/', async (req, res) => {
+    res.render('homepage');
     try {
         const postData = await BlogPost.findAll({
             include: User,
         });
 
-        if(!postData) {
-            res.status(401).json({ message: 'No posts found.'});
-            console.log('No posts found.');
-            //still render homepage even if no posts found?
-            res.render('homepage', {
-                loggedIn: req.session.loggedIn,
-            });
-        }
+        // if(!postData) {
+        //     res.status(400).json({ message: 'No posts found.'});
+        //     console.log('No posts found.');
+        //     //still render homepage even if no posts found?
+        //     res.render('homepage', {
+        //         loggedIn: req.session.loggedIn,
+        //     });
+        // }
 
         const blogPosts = postData.map((blogPosts) => {
             blogPosts.get({ plain: true })
